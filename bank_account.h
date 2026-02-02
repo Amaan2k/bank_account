8f23ef0cbd56aa7fadb94bd15c8706531c8f1d9e
Amaan2k <92322937+Amaan2k@users.noreply.github.com>
2026-02-02 13:19:33 -0500

    Fix TODOs in bank_account.h

--- bank_account.h
+++ bank_account.h
@@ -2,17 +2,19 @@
 #pragma once
 #include <stdexcept>
 
-class BankAccount {
+class BankAccount
+{
     // private class member not visible outside the class
     // private:
     unsigned _balance; // the amount of money in the account
 public:
     // public member functions visible outside the class
     constexpr BankAccount(unsigned balance = 0) // construct account with default balance 0
-        : _balance(balance) // copy initial balance to private member _balance
+        : _balance(balance)                     // copy initial balance to private member _balance
     {
         // throw exception if negative balance
-        if (_balance < 0) {
+        if (_balance < 0)
+        {
             throw std::runtime_error("balance must be non-negative");
         }
     }
@@ -23,17 +25,24 @@ public:
     }
     constexpr unsigned withdraw(unsigned amount)
     {
-		// TODO: throw exception if amount > _balance
+        // throw exception if amount > _balance
+        if (amount > _balance)
+        {
+            throw std::runtime_error("insufficient funds");
+        }
         _balance -= amount;
 
         return _balance;
     }
     constexpr unsigned deposit(unsigned amount)
     {
-		// TODO: throw exception if addition would cause overflow
+        // throw exception if addition would cause overflow
+        if (_balance > ((unsigned)(-1) - amount))
+        {
+            throw std::runtime_error("deposit would cause overflow");
+        }
         _balance += amount;
 
         return _balance;
     }
 }; // <-- classes must have a trailing semicolon
-
